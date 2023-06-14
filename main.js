16a97ec5e8970d2db4a1cb39ff8b39492e2d0359
Batsim03 <baptiste.simon@hotmail.com>
2023-06-14 12:33:08 +0200

    nueva version

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const products = [
-    name
-]
+import { Configuration, OpenAIApi } from "openai"
+const configuration = new Configuration ({
+    apiKey: ProcessingInstruction.env.OPENAI_API_KEY,})
+const openai = new OpenAIApi (configuration)
+export function talkWithChatGPT (prompt) {
+    model: "gpt-3.5-turbo",
+    messages; [{ "role" : "user", "content": prompt }]
+}
+return completion.data.choices[0].text
