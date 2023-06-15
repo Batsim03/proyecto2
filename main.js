c8f85dae3c0bcf2277990deed67533de37f83761
Batsim03 <baptiste.simon@hotmail.com>
2023-06-15 15:20:04 +0200

    otra nueva version

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ export function talkWithChatGPT (prompt) {
     messages; [{ "role" : "user", "content": prompt }]
 }
 return completion.data.choices[0].text
+
+const paragraph = document.querySelector("div");
+const mediaQueryList = window.matchMedia("(max-width: 600px)");
+
+function screenTest(e) {
+  if (e.matches) {
+    /* the viewport is 600 pixels wide or less */
+    paragraph.textContent = "This is a narrow screen — 600px wide or less.";
+    document.body.style.backgroundColor = "pink";
+  } else {
+    /* the viewport is more than 600 pixels wide */
+    paragraph.textContent = "This is a wide screen — more than 600px wide.";
+    document.body.style.backgroundColor = "aquamarine";
+  }
+}
+
+mediaQueryList.addListener(screenTest);
