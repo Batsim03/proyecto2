b98e845bf8d66fd6d234bc2aa59b764d864a62f9
Batsim03 <baptiste.simon@hotmail.com>
2023-06-20 16:26:23 +0200

    otra version más

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-import { Configuration, OpenAIApi } from "openai"
-const configuration = new Configuration ({
-    apiKey: ProcessingInstruction.env.OPENAI_API_KEY,})
-const openai = new OpenAIApi (configuration)
-export function talkWithChatGPT (prompt) {
-    model: "gpt-3.5-turbo",
-    messages; [{ "role" : "user", "content": prompt }]
-}
-return completion.data.choices[0].text
+const AITechnologies1 = {
 
-const paragraph = document.querySelector("div");
-const mediaQueryList = window.matchMedia("(max-width: 600px)");
-
-function screenTest(e) {
-  if (e.matches) {
-    /* the viewport is 600 pixels wide or less */
-    paragraph.textContent = "This is a narrow screen — 600px wide or less.";
-    document.body.style.backgroundColor = "pink";
-  } else {
-    /* the viewport is more than 600 pixels wide */
-    paragraph.textContent = "This is a wide screen — more than 600px wide.";
-    document.body.style.backgroundColor = "aquamarine";
-  }
+    feature1:'available',
+    feature2:'practical',
+    feature3:'economical',
 }
 
-mediaQueryList.addListener(screenTest);
+function functionAEejecutar ()  {
+    AITechnologies1[0];
+ 
+}
