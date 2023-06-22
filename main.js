4717cf6dd6c465cdad7684d5d578e17ef9e5dbc9
Batsim03 <baptiste.simon@hotmail.com>
2023-06-22 11:35:04 +0200

    ultima version

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
+const $btnOcultar = document.querySelector("#btnOcultar"),
+	$btnMostrar = document.querySelector("#btnMostrar"),
+	$parrafo = document.querySelector("#parrafo");
+
+$btnMostrar.addEventListener("click", () => {
+	$parrafo.style.display = "parrafo";
+});
+
+
+$btnOcultar.addEventListener("click", () => {
+	$parrafo.style.display = "none";
+});
 
 
