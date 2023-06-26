b09beae737e0e4630ec9634d073cc40687053d29
Batsim03 <baptiste.simon@hotmail.com>
2023-06-26 18:59:26 +0200

    another day more

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-const $btnOcultar = document.querySelector("#btnOcultar"),
-	$btnMostrar = document.querySelector("#btnMostrar"),
-	$parrafo = document.querySelector("#parrafo");
 
-$btnMostrar.addEventListener("click", () => {
-	$parrafo.style.display = "value";
-});
+let inputValue = '';
+
+const onButtonClicked = () => {
+
+	const Div1 = [
+		'Mejor opción',
+		'ownwer no conocido',
+		'facilidades de resultados'
+	]
+	const listElement = document.createElement ('ul');
+	listElement.className = 'Div1';
+	
+	for (let i=0; i<Div1.length; i++) {
+		const caracteristica = Div1[i];
+		contliElement=docuemnt.createElement('li');
+		listElement.append(listElement);
+	}
+	
+	document.body.append(listElement);
+	
+
+};
+
+const onInputChanged = (input) => {
+inputValue = input.value;
+const inputTextH3 = document.querySelector (`#input-text`);
+inputTextH3.innerText = inputValue;
+
+};
 
 
-$btnOcultar.addEventListener("click", () => {
-	$parrafo.style.display = "none";
-});
 
+	
 
