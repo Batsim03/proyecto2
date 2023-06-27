949d4328490a75b9e18c2f9ffab66ab7ae24911f
Batsim03 <baptiste.simon@hotmail.com>
2023-06-27 16:46:16 +0200

    new version sin funciones

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
 
-let inputValue = '';
+const inputText = "input-text" => {
+function ir() {
+	document.location("id")
+}
 
-const onButtonClicked = () => {
-
-	const Div1 = [
-		'Mejor opción',
-		'ownwer no conocido',
-		'facilidades de resultados'
-	]
-	const listElement = document.createElement ('ul');
-	listElement.className = 'Div1';
-	
-	for (let i=0; i<Div1.length; i++) {
-		const caracteristica = Div1[i];
-		contliElement=docuemnt.createElement('li');
-		listElement.append(listElement);
-	}
-	
-	document.body.append(listElement);
-	
-
-};
-
-const onInputChanged = (input) => {
-inputValue = input.value;
-const inputTextH3 = document.querySelector (`#input-text`);
-inputTextH3.innerText = inputValue;
-
-};
+}
 
 
 
